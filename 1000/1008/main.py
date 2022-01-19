66b3691afec74919e5f6177b2ef0c41cefebef3c
André <andrelu0022@gmail.com>
2022-01-18 21:17:02 -0300

    1008 Done

---
+++ 1000/1008/main.py
@@ -0,0 +1,56 @@
+#Static Methods
+
+import csv
+
+class Item:
+    pay_rate = 0.8 #Discount example, 20% discount
+    all = []
+    def __init__(self, name: str, price: float, quantity: int):
+        # Validating the Values
+        assert price >= 0, f"Price {price} is invalid! Must be greater than 0"
+        assert quantity >= 0, f"Quantity {quantity} is invalid! Must be greater than 0"
+        
+        # Assing the Object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+        # Actions To Execute
+        Item.all.append(self)
+
+    def calculate_total_price(self):
+        return self.price*self.quantity;
+
+    def apply_pay_rate(self):
+        self.price = self.price*self.pay_rate 
+        # self : Instance Level 
+        # <class_name> : Class Level
+
+    # @classmethod # Used to import data from other format
+    # def instantiate_from_csv(cls):
+    #     with open('items.csv', 'r') as f:
+    #         reader = csv.DictReader(f)
+    #         items = list(reader)
+
+    #     TODO: Find a way to fix this
+    #     for item in items: 
+    #          Item(
+    #              name=item.get('name'),
+    #              price=float(item.get('price')), 
+    #              quantity=int(item.get('quantity')),
+    #          )
+
+    @staticmethod #Usado para referênciar os objetos instânciados, static methods do not need an paramether like self or cls
+    def is_integer(num):
+        if isinstance(num, float):
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
+
+    def __repr__(self):
+        return f"Item('{self.name}' , {self.price} , {self.quantity})"
+
+
+print(Item.is_integer(7.0))
